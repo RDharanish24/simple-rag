a7d01405cce0fe62cba2ef34f8f540675a020bc3
RDharanish24 <dharanish2404@gmail.com>
2026-06-01 22:41:56 +0530

    updated

--- main.py
+++ main.py
@@ -3,4 +3,10 @@ from sentence_transformers import SentenceTransformer
 from google import genai
 
 model=SentenceTransformer("all-MiniLM-L6-v2")
-with open("")
+with open(r"C:\Users\DELL\Documents\ml projects\simple-RAG\genai_notes.txt","r",encoding="utf-8") as a:
+    notes=a.read().splitlines()
+
+embeddings=model.encode(notes)
+
+print(f"Number of documents processed: {len(notes)}")
+print(f"Shape of embeddings array: {embeddings.shape}")
