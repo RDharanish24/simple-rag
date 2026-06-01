f2de59da67c6254fd3aba7b3702da74d8e68d0d8
RDharanish24 <dharanish2404@gmail.com>
2026-06-01 22:34:08 +0530

    first commit

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 import numpy as np
-from sentence_transformers import sentence_transformer
+from sentence_transformers import SentenceTransformer
 from google import genai
+
+model=SentenceTransformer("all-MiniLM-L6-v2")
+with open("")
