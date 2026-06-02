0c23b44be803d616333462da896cef4b32a609a4
RDharanish24 <dharanish2404@gmail.com>
2026-06-02 22:17:08 +0530

    updated

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import numpy as np
 from sentence_transformers import SentenceTransformer
 from google import genai
-
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 model=SentenceTransformer("all-MiniLM-L6-v2")
 with open(r"C:\Users\DELL\Documents\ml projects\simple-RAG\genai_notes.txt","r",encoding="utf-8") as a:
-    notes=a.read().splitlines()
+    raw_text=a.read()
 
+text_splitter=RecursiveCharacterTextSplitter(chunk_size=100,chunk_overlap=50,separators=["\n\n","\n"," ",""]  )
+notes=text_splitter.split_text(raw_text)
 embeddings=model.encode(notes)
 
 print(f"Number of documents processed: {len(notes)}")
