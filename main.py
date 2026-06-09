f317da01a89eedb3f01195e2d6e5ec823fda9ea3
RDharanish24 <dharanish2404@gmail.com>
2026-06-09 13:34:35 +0530

    Add chromadb integration for document storage and retrieval; create .gitignore for local_db

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 from sentence_transformers import SentenceTransformer
 from google import genai
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+import chromadb
 model=SentenceTransformer("all-MiniLM-L6-v2")
 with open(r"C:\Users\DELL\Documents\ml projects\simple-RAG\genai_notes.txt","r",encoding="utf-8") as a:
     raw_text=a.read()
@@ -10,5 +11,19 @@ text_splitter=RecursiveCharacterTextSplitter(chunk_size=100,chunk_overlap=50,sep
 notes=text_splitter.split_text(raw_text)
 embeddings=model.encode(notes)
 
-print(f"Number of documents processed: {len(notes)}")
-print(f"Shape of embeddings array: {embeddings.shape}")
+client=chromadb.PersistentClient(path="./local_db")
+
+collection=client.get_or_create_collection(
+    name="document_store",
+    metadata={"hnsw:space":"cosine"}
+)
+
+collection.add(documents=notes,embeddings=embeddings,ids=[f"id{i}" for i in range(len(notes))])
+
+query=input("enter your query")
+query_embedding=model.encode(query)
+query_results=collection.query(query_embeddings=[query_embedding],n_results=3)
+
+retrieved_docs= query_results["documents"][0]
+context = " ".join(retrieved_docs)
+print(context)
