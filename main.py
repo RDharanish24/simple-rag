578d45e0651100cd83b2dd95e36cd1e60d46bdb6
RDharanish24 <dharanish2404@gmail.com>
2026-06-09 21:38:40 +0530

    Implement FastAPI endpoints for query handling and response generation

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import ollama
 import chromadb
 from sentence_transformers import SentenceTransformer
+from fastapi import FastAPI
+from pydantic import BaseModel
+app=FastAPI()
 model=SentenceTransformer("all-MiniLM-L6-v2")
 client=chromadb.PersistentClient(path="./local_db")
 
@@ -9,15 +12,32 @@ collection=client.get_or_create_collection(
     metadata={"hnsw:space":"cosine"}
 )
 
-query=input("enter your query")
-query_embedding=model.encode(query).tolist()
-query_results=collection.query(query_embeddings=[query_embedding],n_results=3)
+class Queryrequest(BaseModel):
+    query:str
 
-retrieved_docs= query_results["documents"][0]
-context = " ".join(retrieved_docs)
+@app.get("/")
+def home():
+    return {"message":"RAG API is running"}
 
-prompt = f"""
-Use the below context to answer the question.
+@app.post("/ask")
+
+def ask_question(request:Queryrequest):
+    query=request.query
+    query_embedding=model.encode(query).tolist()
+    query_results=collection.query(query_embeddings=[query_embedding],n_results=3)
+    retrieved_docs= query_results.get("documents",[[]])[0]
+    if not retrieved_docs:
+        return {"answer":"no relevent answers found"}
+    context = " ".join(retrieved_docs)
+
+
+
+
+    prompt = f"""
+You are an AI assistant.
+
+Answer ONLY using the provided context.
+If the answer is not in the context, say "I don't know".
 
 Context:
 {context}
@@ -28,6 +48,6 @@ Question:
 Answer:
 """
 
-response=ollama.generate(model="llama3.1",prompt=prompt)
-print(response['response'])
+    response=ollama.generate(model="llama3.1",prompt=prompt)
+    return {"answer":response.get("response","no response")}
 
